0f5d4b09561cdd021e823a88133b09388148161c
aneek ghosh <aneekghosh25@gmail.com>
2022-06-06 10:28:56 +0530

    minor update on Table.js

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,10 +2,11 @@ import { useState,useEffect } from 'react';
 import Cross from '../cross/Cross';
 import Naught from '../naught/Naught';
 import './Table.css'
-import ReactDOM from 'react-dom';
 import {check} from  '../../utils/game.js';
 const Table = () => {
     console.log('rendering table')
+
+    const options = {"-1" : null , "0" : <Naught/>, "1" : <Cross/>}
 
     const [table,setTable] = useState([[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]])
     const [move,setMove] = useState(1);
@@ -17,57 +18,37 @@ const Table = () => {
         index: -1
     })
 
-    //renders the table
+    //renders the cut line
     useEffect(() => {
         console.log('in use effect')
 
-        const moves = {"-1" : null , "0" : <Naught/>, "1" : <Cross/>}
+        const cut = document.querySelector('#cut')
 
-        const diagonals = document.querySelectorAll('.diagonal')
+        const {winner,type,index} = result
 
-        diagonals.forEach(diagonal => {
-            const {winner,type,index} = result
+        if(winner === -1)
+            cut.style.display = "none"
+        else
+        {
+            cut.style.display = "block"
 
-            if(winner === -1)
-                diagonal.style.display = "none"
-            else
-            {
-                diagonal.style.display = "block"
+            const side = cut.parentElement.offsetWidth / 3
 
-                const side = diagonal.parentElement.offsetWidth / 3
+            const rot = type === 'row' ? 0 : type === 'col' ? 90 : index * 45
 
-                console.log(side)
-                
-                const rot = type === 'row' ? 0 : type === 'col' ? 90 : index * 45
+            const scale = type === 'diagonal' ? 1.4 : 1
 
-                const scale = type === 'diagonal' ? 1.4 : 1
+            const dir = index === -1 ? 'right' : 'left'
 
-                const dir = index === -1 ? 'right' : 'left'
+            let trans = type !== 'diagonal' ? (index+0.5) * side : 0
 
-                let trans = type !== 'diagonal' ? (index+0.5) * side : 0
+            if(type === 'col') 
+                trans = -1 * trans
 
-                if(type === 'col') 
-                    trans = -1 * trans
-
-                diagonal.style.transform = `rotate(${rot}deg) scale(${scale}) translate(0px,${trans}px)`
-                diagonal.style.transformOrigin = 'top ' + dir
-            }
-            
-        })
-
-        const boxNodes = document.querySelectorAll('.box');
-
-        boxNodes.forEach((boxNode,i)=>{
-            boxNode.style.pointerEvents = move ? "all" : "none";
-
-            const r = parseInt(i/3)
-            const c = parseInt(i%3)
-
-            // eslint-disable-next-line
-            ReactDOM.render(moves[table[r][c]],boxNode)
-        })
-
-    }, [move,table,result]);
+            cut.style.transform = `rotate(${rot}deg) scale(${scale}) translate(0px,${trans}px)`
+            cut.style.transformOrigin = 'top ' + dir
+        }
+    }, [result]);
 
     //calls the computer move
     useEffect(()=>{
@@ -92,7 +73,10 @@ const Table = () => {
             setGameOver(true)
             setResult(res)
         }
-    },[move,table])
+        else if(moves === 9){
+            setGameOver(true)
+        }
+    },[move,table,moves])
 
     const computerMove = ()=>{
         if(gameOver || moves === 9)
@@ -132,24 +116,41 @@ const Table = () => {
 
     return ( 
         <div className="h-100 w-100">
-            <div className='diagonal border bg-dark border-dark border-1'></div>
+            <div className='border bg-dark border-dark border-1' id="cut"></div>
             <div className="row mx-0 h-30 border-bottom border-3 border-dark">
                 <div data-boxnumber="0" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box" id="box">
+                    {options[table[0][0]]}
                 </div>
-                <div data-boxnumber="1" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box"></div>
-                <div data-boxnumber="2" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box"></div>
+                <div data-boxnumber="1" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box">
+                    {options[table[0][1]]}
+                </div>
+                <div data-boxnumber="2" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box">
+                    {options[table[0][2]]}
+                </div>
             </div>
             <div className="row mx-0  h-30 border-bottom border-3 border-dark">
-                <div data-boxnumber="3" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box"></div>
-                <div data-boxnumber="4" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box"></div>
-                <div data-boxnumber="5" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box"></div>
+                <div data-boxnumber="3" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box">
+                    {options[table[1][0]]}
+                </div>
+                <div data-boxnumber="4" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box">
+                    {options[table[1][1]]}
+                </div>
+                <div data-boxnumber="5" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box">
+                    {options[table[1][2]]}
+                </div>
             </div>
             <div className="row mx-0 h-30">
-                <div data-boxnumber="6" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box"></div>
-                <div data-boxnumber="7" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box"></div>
-                <div data-boxnumber="8" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box"></div>
+                <div data-boxnumber="6" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box">
+                    {options[table[2][0]]}
+                </div>
+                <div data-boxnumber="7" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 border-end border-3 border-dark box">
+                    {options[table[2][1]]}
+                </div>
+                <div data-boxnumber="8" onClick={(e)=>{ handleClick(e) }} className="col-4 p-0 box">
+                    {options[table[2][2]]}
+                </div>
             </div>
-            <p>{!gameOver ? (move ? "your turn" : "computer's turn") : (result.winner !== -1 && result.winner)}</p>
+            <p>{!gameOver ? (move ? "your turn" : "computer's turn") : result.winner}</p>
         </div>
      );
 }
